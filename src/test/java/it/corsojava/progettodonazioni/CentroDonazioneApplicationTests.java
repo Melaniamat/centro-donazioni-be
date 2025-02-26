dc6bc7d3ce2566c0b6f24bb65d0bdb8db39cdb4e
gabriele_laca <gabrielelaca34@gmail.com>
2025-02-26 10:38:43 +0100

    Primo commit

---
+++ src/test/java/it/corsojava/progettodonazioni/CentroDonazioneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.corsojava.progettodonazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDonazioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
