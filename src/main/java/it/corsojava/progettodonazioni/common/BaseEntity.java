cdb2f4b6f882cba76f9f6c71a26c4f579b540bf7
Melaniamat <melaniamatacena.97@gmail.com>
2026-05-23 21:00:42 +0200

    refactor entities: added baseEntity + Lombok

---
+++ src/main/java/it/corsojava/progettodonazioni/common/BaseEntity.java
@@ -0,0 +1,20 @@
+package it.corsojava.progettodonazioni.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    protected BaseEntity (){}
+}
---
+++ src/main/java/it/corsojava/progettodonazioni/common/BaseRepository.java
@@ -0,0 +1,13 @@
+package it.corsojava.progettodonazioni.common;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.UUID;
+
+@NoRepositoryBean
+public interface BaseRepository<E extends BaseEntity> extends JpaRepository<E, Long>,
+        JpaSpecificationExecutor<E> {
+
+}
